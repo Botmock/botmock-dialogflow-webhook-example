116265a83e539300d77d1c7ed5f12f5d0b509eb5
RAHUL <rahul.chauhan049@gmail.com>
2020-11-19 03:05:39 +0530

    remove unwanted dependencies and added new method without using dialogflow-fulfilment lib

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const app = express();
-const dfff = require('dialogflow-fulfillment')
-const {BotmockUtils} = require("@botmock/sdk"); // Or replace with path to botmock utils lib
 const functionsMap = require('./function');
 
-app.post('/', express.json(), (request, response)=> {
-    const agent = new dfff.WebhookClient({
-        request,
-        response
-    })
 
-    let intentMap = new Map();
-
-    intentMap = BotmockUtils.mapIntents(intentMap, functionsMap);
-
-    agent.handleRequest(intentMap)
+app.post('/', express.json(), (req, res)=> {
+    let intentName = req.body.queryResult.intent.displayName
+    let response = functionsMap[intentName](req)
+    res.send(response._response)
 })
-let port = process.env.PORT || 3000
 
+let port = process.env.PORT || 3000
 app.listen(port, ()=> {
     console.log('Server is live at port '+ port)
 })
